21810445737f9615aaf8e96b67ab29b4ae6e63c4
Harsh Sangwan <hsangwan2002@gmail.com>
2021-12-22 13:36:20 +0530

    init

---
+++ Foundation/Recursion & Backtracking/Recursion With ArrayList/Class - 24/getSubsequence.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class getSubsequence {
+
+    public static ArrayList<String> subsequence(String str) {
+
+        if (str.length() == 0) {
+
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        ArrayList<String> rres = subsequence(ros);
+
+        ArrayList<String> mres = new ArrayList<>();
+
+        for (String s : rres)
+            mres.add("" + s);
+
+        for (String s : rres)
+            mres.add(ch + s);
+
+        return mres;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+
+        ArrayList<String> ans = subsequence(str);
+        System.out.println(ans);
+        scn.close();
+    }
+}
