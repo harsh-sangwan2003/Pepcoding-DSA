3fed0985b12096ad7b20002f1e14096eb93179c1
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-01-12 11:17:25 +0530

    init

---
+++ Foundation/Basic Data Structures/Linked Lists/Class - 33/Extra/display_linked_list.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class display_linked_list {
+
+    public static class Node {
+
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList {
+
+        Node head;
+        Node tail;
+        int size;
+
+        void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            temp.next = null;
+
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+
+            size++;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public void display() {
+
+            if (size == 0)
+                return;
+
+            helper(head);
+        }
+
+        public void helper(Node node) {
+
+            if (node == null)
+                return;
+
+            System.out.println(node.data);
+
+            helper(node.next);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        LinkedList list = new LinkedList();
+
+        String str = br.readLine();
+
+        while (str.equals("quit") == false) {
+            if (str.startsWith("addLast")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            } else if (str.startsWith("size")) {
+                System.out.println(list.size());
+            } else if (str.startsWith("display")) {
+                list.display();
+            }
+            str = br.readLine();
+        }
+    }
+}
