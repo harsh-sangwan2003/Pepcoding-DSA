51a04ee6be7cd986dad885d7a3b22940ac20161a
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-02-28 09:59:27 +0530

    added

--- Foundation/Dp - Rajneesh Sir/Decode Ways/memoised.java
+++ Foundation/Dp - Rajneesh Sir/Decode Ways/memoised.java
@@ -1,7 +1,6 @@
 //Time - O(n)
 //Space - O(n)
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class memoised {
@@ -9,33 +8,31 @@ public class memoised {
     public static int numDecodings(String s) {
 
         int n = s.length();
-
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
 
-        return numDecodings(s, n, dp);
+        return helper(s, n, dp);
     }
 
-    public static int numDecodings(String s, int n, int[] dp) {
+    public static int helper(String s, int n, int[] dp) {
 
         if (n == 0)
             return dp[n] = 1;
 
-        if (dp[n] != -1)
+        if (dp[n] != 0)
             return dp[n];
 
         int count = 0;
-
         if (s.charAt(n - 1) != '0')
-            count += numDecodings(s, n - 1, dp);
+            count += helper(s, n - 1, dp);
 
-        if (n > 1) {
-            int val1 = Integer.parseInt(s.charAt(n - 2) + "");
-            int val2 = Integer.parseInt(s.charAt(n - 1) + "");
+        if (n - 1 > 0) {
+
+            int val2 = s.charAt(n - 1) - '0';
+            int val1 = s.charAt(n - 2) - '0';
             int val = val1 * 10 + val2;
 
             if (val >= 10 && val <= 26)
-                count += numDecodings(s, n - 2, dp);
+                count += helper(s, n - 2, dp);
         }
 
         return dp[n] = count;
