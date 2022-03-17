7fef486c4717b1ea987d1421b999102ccd0abb81
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-03-17 22:05:07 +0530

    added

---
+++ Foundation/Basic Data Structures/Stacks & Queues/Class - 56/lbd.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class lbd {
+
+    public static void main(String... args) {
+
+        int[][] arr = { { 1, 3 }, { 6, 10 }, { -3, 13 }, { 5, 20 } };
+
+        Arrays.sort(arr, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        for (int[] d : arr) {
+
+            System.out.println(d[0] + " " + d[1]);
+        }
+    }
+}
