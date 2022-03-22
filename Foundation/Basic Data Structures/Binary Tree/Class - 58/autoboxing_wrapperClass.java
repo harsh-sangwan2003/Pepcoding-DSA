fe70b4b5587d6c2fa26e95e34b2073e84649f2f8
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-03-22 21:56:01 +0530

    added

---
+++ Foundation/Basic Data Structures/Binary Tree/Class - 58/autoboxing_wrapperClass.java
@@ -0,0 +1,23 @@
+public class autoboxing_wrapperClass {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+        Integer[] brr = new Integer[5];
+
+        for (int val : arr)
+            System.out.print(val + " ");
+
+        System.out.println();
+
+        for (Integer val : brr)
+            System.out.print(val + " ");
+
+        Integer i = new Integer(8);
+
+        System.out.println();
+
+        System.out.println(i);
+
+    }
+}
