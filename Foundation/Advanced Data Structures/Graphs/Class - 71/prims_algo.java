5f9ada74ead8d9d6e64a130efb37d057389c0fb5
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-26 21:41:22 +0530

    added

---
+++ Foundation/Advanced Data Structures/Graphs/Class - 71/prims_algo.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class prims_algo {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+   
+   public static class Pair{
+       
+       int src;
+       int av;
+       int wt;
+       
+       Pair(int src, int av, int wt){
+           
+           this.src = src;
+           this.av = av;
+           this.wt = wt;
+       }
+   }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      // write your code here
+      PriorityQueue<Pair> pq = new PriorityQueue<>((p1,p2)->{
+          
+          return p1.wt-p2.wt;
+      });
+      boolean[] visit = new boolean[vtces];
+      
+      pq.add(new Pair(0,-1,0));
+      
+      while(pq.size()!=0){
+          
+          Pair rm = pq.remove();
+          
+          if(visit[rm.src])
+          continue;
+          
+          visit[rm.src] = true;
+          
+          if(rm.av!=-1)
+          System.out.println("["+rm.src+"-"+rm.av+"@"+rm.wt+"]");
+          
+          for(Edge e : graph[rm.src]){
+              
+              if(!visit[e.nbr])
+              pq.add(new Pair(e.nbr,rm.src,e.wt));
+          }
+          
+      }
+   }
+
+}
