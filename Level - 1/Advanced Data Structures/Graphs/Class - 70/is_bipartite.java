ff0836175f3b82833b2abc3740a19311285d71bc
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-29 12:26:21 +0530

    added

--- Level - 1/Advanced Data Structures/Graphs/Class - 70/is_bipartite.java
+++ Level - 1/Advanced Data Structures/Graphs/Class - 70/is_bipartite.java
@@ -1,78 +1,82 @@
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Queue; 
+
 class Solution {
-    
-    public class Pair{
-        
+
+    public class Pair {
+
         int vtx;
         int clr;
-        
-        Pair(){
-            
+
+        Pair() {
+
         }
-        
-        Pair(int vtx, int clr){
-            
+
+        Pair(int vtx, int clr) {
+
             this.vtx = vtx;
             this.clr = clr;
         }
-        
+
     }
-    
+
     public boolean isBipartite(int[][] graph) {
-        
+
         int vtces = graph.length;
         int[] visit = new int[vtces];
-        Arrays.fill(visit,-1);
-        
-        for(int v=0; v<vtces; v++){
-            
-            if(visit[v]==-1){
-                
-                boolean flag = check(graph,v,visit);
-                
-                if(!flag)
+        Arrays.fill(visit, -1);
+
+        for (int v = 0; v < vtces; v++) {
+
+            if (visit[v] == -1) {
+
+                boolean flag = check(graph, v, visit);
+
+                if (!flag)
                     return false;
             }
         }
-        
+
         return true;
     }
-    
-    public boolean check(int[][] graph, int v, int[] visit){
-        
+
+    public boolean check(int[][] graph, int v, int[] visit) {
+
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(v,0));
-        
-        while(q.size()!=0){
-            
+        q.add(new Pair(v, 0));
+
+        while (q.size() != 0) {
+
             int size = q.size();
-            
+
             Pair top = q.remove();
-                
-                if(visit[top.vtx]==-1){
-                    
-                    visit[top.vtx] = top.clr;
+
+            if (visit[top.vtx] == -1) {
+
+                visit[top.vtx] = top.clr;
+            }
+
+            else {
+
+                if (visit[top.vtx] == top.clr)
+                    continue;
+
+                else
+                    return false;
+            }
+
+            int src = top.vtx;
+
+            for (int e : graph[src]) {
+
+                if (visit[e] == -1) {
+
+                    q.add(new Pair(e, 1 - top.clr));
                 }
-                
-                else{
-                    
-                    if(visit[top.vtx]==top.clr)
-                        continue;
-                    
-                    else
-                        return false;
-                }
-                
-                int src = top.vtx;
-                
-                for(int e : graph[src]){
-                    
-                    if(visit[e]==-1){
-                        
-                        q.add(new Pair(e,1-top.clr));
-                    }
-                }
+            }
         }
-        
+
         return true;
     }
 }
